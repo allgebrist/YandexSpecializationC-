d8e7830dcf6ddeb62a6d194c6e0f68f8f4d39354
Allan A. Zea <allan.zea@ciens.ucv.ve>
2019-12-09 00:07:23 +0100

    added comments

--- Основы разработки на C++: Белый пояс/12. Разворот последовательности — 2.cpp
+++ Основы разработки на C++: Белый пояс/12. Разворот последовательности — 2.cpp
@@ -14,7 +14,7 @@ using namespace std;
 возвращающую копию вектора v, в которой числа переставлены в 
 обратном порядке.
 */
-
+// Решение с Two-Pointers
 vector<int> Reversed(const vector<int>& v) {
 	vector<int> v_copy(v.size());
 	if (v.size() != 0 && v.size() != 1) {
@@ -29,6 +29,18 @@ vector<int> Reversed(const vector<int>& v) {
 	}
 	return v_copy;
 }
+
+/*
+Простое решение с push_back:
+vector<int> Reversed(const vector<int>& input)
+{
+    vector<int> result;
+    for (int i = input.size() - 1; i >= 0; --i) {
+        result.push_back(input[i]);
+    }
+    return result;
+}
+*/
 
 int main() {
 	ios_base::sync_with_stdio(false);
